9c8bf6eed1fcdf93a73daa148a86c8f5f4277eac
Aishi Jain <162431264+Aishi-Jain@users.noreply.github.com>
2024-03-17 11:33:30 +0530

    finding the nth Fibonacci number in Fibonacci.java

--- src/aishi/Fibonacci.java
+++ src/aishi/Fibonacci.java
@@ -1,7 +1,27 @@
 package aishi;
-
+import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
-        
+        /*
+         Fbonacci numbers : a series of numbers in which each number
+        ( Fibonacci number ) is the sum of the two preceding numbers.
+        Ex :- 0,1,1,2,3,5,8,13…
+         */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while(count <= n)
+        {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count ++;
+        }
+
+        System.out.println(b);
+
+
     }
 }
